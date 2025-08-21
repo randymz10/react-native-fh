8ff77dd9f8137a8ac535bf1d4da5bb140d91fd0b
randymz10 <randymz10@gmail.com>
2025-08-21 10:05:00 -0300

    Initialize counter-app with Expo and TypeScript configuration

---
+++ 03-counter-app/App.tsx
@@ -0,0 +1,47 @@
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import FAB from "./components/FAB";
+
+export default function App() {
+  const [count, setCount] = useState(10);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.textHuge}>{count}</Text>
+
+      {/* <Pressable
+        style={styles.floatingButton}
+        onPress={() => setCount(count + 1)}
+        onLongPress={() => {
+          setCount(0);
+        }}
+      >
+        <Text style={{ color: "white", fontSize: 20 }}>+1</Text>
+      </Pressable> */}
+      <FAB label={"Reset"} onPress={() => setCount(0)} position="left" />
+
+      <FAB
+        label={"+1"}
+        onPress={() => setCount(count + 1)}
+        onLongPress={() => setCount(0)}
+      />
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  textHuge: {
+    fontSize: 120,
+    fontWeight: 100,
+  },
+});
---
+++ 03-counter-app/components/FAB.tsx
@@ -0,0 +1,53 @@
+import { Text, StyleSheet, Pressable } from "react-native";
+
+interface Props {
+  label: string;
+
+  position?: "left" | "right";
+  //Methods
+  onPress?: () => void;
+  onLongPress?: () => void;
+}
+
+export default function FAB({
+  label,
+  onPress,
+  onLongPress,
+  position = "right",
+}: Props) {
+  return (
+    <Pressable
+      style={({ pressed }) => [
+        styles.floatingButton,
+        position === "left" ? styles.positionLeft : styles.positionRight,
+        pressed ? { opacity: 0.7 } : { opacity: 1 },
+      ]}
+      onPress={onPress}
+      onLongPress={onLongPress}
+    >
+      <Text style={{ color: "white", fontSize: 20 }}>{label}</Text>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  floatingButton: {
+    position: "absolute",
+    bottom: 50,
+
+    backgroundColor: "#65558F",
+    padding: 20,
+    borderRadius: 15,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.3,
+    elevation: 3,
+    shadowRadius: 4,
+  },
+  positionRight: {
+    right: 20,
+  },
+  positionLeft: {
+    left: 20,
+  },
+});
