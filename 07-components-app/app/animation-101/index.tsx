adfe9947a2295dfd045be49e712a16578392e2c8
randymz10 <randymz10@gmail.com>
2025-09-18 12:19:13 -0300

    feat: enhance animation functionality; add vertical translation to fadeIn and fadeOut effects

--- 07-components-app/app/animation-101/index.tsx
+++ 07-components-app/app/animation-101/index.tsx
@@ -1,16 +1,25 @@
-import { Animated, View } from "react-native";
+import { useRef } from "react";
+import { Animated, Easing } from "react-native";
 
 import ThemedButton from "../../presentation/shared/ThemedButton";
 import ThemedView from "@/presentation/shared/ThemedView";
-import { useRef } from "react";
 
 const Animation101Screen = () => {
   const animatedOpacity = useRef(new Animated.Value(0)).current;
+  const animatedTop = useRef(new Animated.Value(-100)).current;
+
   const fadeIn = () => {
     Animated.timing(animatedOpacity, {
       toValue: 1,
       duration: 300,
       useNativeDriver: true,
+    }).start();
+
+    Animated.timing(animatedTop, {
+      toValue: 0,
+      duration: 700,
+      useNativeDriver: true,
+      easing: Easing.elastic(3),
     }).start();
   };
 
@@ -19,14 +28,20 @@ const Animation101Screen = () => {
       toValue: 0,
       duration: 300,
       useNativeDriver: true,
-    }).start();
+      // }).start(() => animatedTop.setValue(-100));
+    }).start(() => animatedTop.resetAnimation());
   };
 
   return (
     <ThemedView margin className="justify-center items-center flex-1">
       <Animated.View
         className="bg-light-secondary dark: bg-dark-secondary rounded-xl"
-        style={{ width: 150, height: 150, opacity: animatedOpacity }}
+        style={{
+          width: 150,
+          height: 150,
+          opacity: animatedOpacity,
+          transform: [{ translateY: animatedTop }],
+        }}
       />
 
       <ThemedButton className="my-5" onPress={fadeIn}>
