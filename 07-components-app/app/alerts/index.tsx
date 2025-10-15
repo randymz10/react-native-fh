3b66c0dd7da52e8ac198ad0ee2ea7a45c9df2c72
randymz10 <randymz10@gmail.com>
2025-10-14 22:14:14 -0300

    feat: implement AlertsScreen with two and three button alert functionality using ThemedButton

--- 07-components-app/app/alerts/index.tsx
+++ 07-components-app/app/alerts/index.tsx
@@ -1,10 +1,47 @@
-import { View, Text } from 'react-native';
+import ThemedButton from "@/presentation/shared/ThemedButton";
+import ThemedView from "@/presentation/shared/ThemedView";
+import { Alert, SafeAreaView } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 const AlertsScreen = () => {
+  const createTwoButtonAlert = () =>
+    Alert.alert("Alert Title", "My Alert Msg", [
+      {
+        text: "Cancel",
+        onPress: () => console.log("Cancel Pressed"),
+        style: "cancel",
+      },
+      { text: "OK", onPress: () => console.log("OK Pressed") },
+    ]);
+
+  const createThreeButtonAlert = () =>
+    Alert.alert("Alert Title", "My Alert Msg", [
+      {
+        text: "Ask me later",
+        onPress: () => console.log("Ask me later pressed"),
+      },
+      {
+        text: "Cancel",
+        onPress: () => console.log("Cancel Pressed"),
+        style: "cancel",
+      },
+      { text: "OK", onPress: () => console.log("OK Pressed") },
+    ]);
+
   return (
-    <View>
-      <Text>AlertsScreen</Text>
-    </View>
+    <ThemedView>
+      <SafeAreaProvider>
+        <SafeAreaView>
+          <ThemedButton className="mb-4 mx-5" onPress={createTwoButtonAlert}>
+            2-Button Alert
+          </ThemedButton>
+          <ThemedButton className="mb-4 mx-5" onPress={createThreeButtonAlert}>
+            3-Button Alert
+          </ThemedButton>
+        </SafeAreaView>
+      </SafeAreaProvider>
+      );
+    </ThemedView>
   );
 };
 export default AlertsScreen;
